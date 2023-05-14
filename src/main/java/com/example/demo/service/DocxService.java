9afd07479a0c5adaf76f46eb15435e9f4a6cd257
phamducdat <phamducdat2402@gmail.com>
2023-05-14 16:06:39 +0700

    fix: mapping value to templateRow

--- src/main/java/com/example/demo/service/DocxService.java
+++ src/main/java/com/example/demo/service/DocxService.java
@@ -2,7 +2,9 @@ package com.example.demo.service;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import org.apache.poi.xwpf.usermodel.*;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTc;
@@ -10,7 +12,6 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class DocxService {
 
@@ -20,30 +21,28 @@ public class DocxService {
         XWPFTableRow templateRow = table.getRow(templateRowPosition); // Assuming the first row is the template row
 
         ArrayList<String> cellKeys = new ArrayList<>();
-        for (XWPFTableCell cell: templateRow.getTableCells()) {
+        for (XWPFTableCell cell : templateRow.getTableCells()) {
             cellKeys.add(cell.getText());
         }
-//        ArrayList<String> cellKeys = new ArrayList<String>(){{
-//            add("##stt##");
-//            add("##ten_san_pham##");
-//            add("##dvt##");
-//            add("##sl##");
-//            add("##don_gia##");
-//            add("##thanh_tien##");
-//        }};
+        setCellsStringValue(cellKeys, values.get(0).getAsJsonObject(), templateRow);
+
         for (int i = 1; i < values.size(); i++) {
             JsonObject value = values.get(i).getAsJsonObject();
             XWPFTableRow newRow = new XWPFTableRow(templateRow.getCtRow(), table);
             table.addRow(newRow, i);
-            for (int j = 0; j < newRow.getTableCells().size(); j++) {
-                XWPFTableCell cell = newRow.getCell(j);
-                CTTc newCellCTTc = cell.getCTTc();
-                for (CTP ctp : newCellCTTc.getPList()) {
-                    for (CTR ctr : ctp.getRList()) {
-                        for (CTText ctText : ctr.getTList()) {
-                            if (value.get(cellKeys.get(j)) != null) {
-                                ctText.setStringValue(value.get(cellKeys.get(j)).getAsString());
-                            }
+            setCellsStringValue(cellKeys, value, newRow);
+        }
+    }
+
+    private static void setCellsStringValue(ArrayList<String> cellKeys, JsonObject value, XWPFTableRow newRow) {
+        for (int j = 0; j < newRow.getTableCells().size(); j++) {
+            XWPFTableCell cell = newRow.getCell(j);
+            CTTc newCellCTTc = cell.getCTTc();
+            for (CTP ctp : newCellCTTc.getPList()) {
+                for (CTR ctr : ctp.getRList()) {
+                    for (CTText ctText : ctr.getTList()) {
+                        if (value.get(cellKeys.get(j)) != null) {
+                            ctText.setStringValue(value.get(cellKeys.get(j)).getAsString());
                         }
                     }
                 }
