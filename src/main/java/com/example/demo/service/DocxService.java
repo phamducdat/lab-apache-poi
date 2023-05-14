c7f82c4aa4d2892ee1767c1e4be21abed38f07e3
phamducdat <phamducdat2402@gmail.com>
2023-05-14 15:59:55 +0700

    fix: gen cell keys from template row

--- src/main/java/com/example/demo/service/DocxService.java
+++ src/main/java/com/example/demo/service/DocxService.java
@@ -18,14 +18,19 @@ public class DocxService {
     public void addTableRow(XWPFTable table, int templateRowPosition, JsonArray values) {
         // Get the existing row to clone its style
         XWPFTableRow templateRow = table.getRow(templateRowPosition); // Assuming the first row is the template row
-        ArrayList<String> cellKeys = new ArrayList<String>(){{
-            add("##stt##");
-            add("##ten_san_pham##");
-            add("##dvt##");
-            add("##sl##");
-            add("##don_gia##");
-            add("##thanh_tien##");
-        }};
+
+        ArrayList<String> cellKeys = new ArrayList<>();
+        for (XWPFTableCell cell: templateRow.getTableCells()) {
+            cellKeys.add(cell.getText());
+        }
+//        ArrayList<String> cellKeys = new ArrayList<String>(){{
+//            add("##stt##");
+//            add("##ten_san_pham##");
+//            add("##dvt##");
+//            add("##sl##");
+//            add("##don_gia##");
+//            add("##thanh_tien##");
+//        }};
         for (int i = 1; i < values.size(); i++) {
             JsonObject value = values.get(i).getAsJsonObject();
             XWPFTableRow newRow = new XWPFTableRow(templateRow.getCtRow(), table);
@@ -42,38 +47,6 @@ public class DocxService {
                         }
                     }
                 }
-            }
-        }
-    }
-
-
-//    public void mappingValues(XWPFTable table, JsonArray values) {
-//        for (int i = 1; i < values.size() + 1; i++) {
-//            XWPFTableRow row = table.getRow(i);
-//            for (XWPFTableCell cell : row.getTableCells()) {
-//                CTTc newCellCTTc = cell.getCTTc();
-//                for (CTP ctp: newCellCTTc.getPList()) {
-//                    for (CTR ctr: ctp.getRList()) {
-//                        for (CTText ctText: ctr.getTList()) {
-//                            System.out.println("dat with ctext = "+ ctText.getStringValue());
-//                            ctText.setStringValue(UUID.randomUUID().toString());
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-    private void replaceTextInParagraphs(List<XWPFParagraph> paragraphs) {
-        for (XWPFParagraph paragraph : paragraphs) {
-            for (int i = 0; i < paragraph.getRuns().size(); i++) {
-                XWPFRun run = paragraph.getRuns().get(i);
-                String replacedText = UUID.randomUUID().toString();
-                XWPFRun newRun = paragraph.insertNewRun(i + 1);
-                newRun.getCTR().set(run.getCTR());
-                newRun.setText(replacedText, 0);
-                paragraph.removeRun(i);
-                i--; // Decrement the index as we just removed a run
             }
         }
     }
